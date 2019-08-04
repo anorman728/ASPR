c8bc2431b9a341e45cb8fe0d0f570cad2c17c80a
Andrew Norman <anorman728@gmail.com>
2019-08-04 19:40:38 +0000

    Added test-specific settings options.

--- phpunit_server.php
+++ phpunit_server.php
@@ -1,4 +1,7 @@
 <?php
+// Almost never add anything to GLOBALS.
+$GLOBALS['isTest'] = true;
+
 // Everything needed for phpunit bootstrapping.
 
 include_once(__DIR__ . '/settings.php');
@@ -6,6 +9,3 @@ include_once(__DIR__ . '/settings.php');
 include_once(__DIR__ . '/commonfunctions.php');
 
 include_once(__DIR__ . '/autoload.php');
-
-// Almost never add anything to GLOBALS.
-$GLOBALS['isTest'] = true;
--- settings.php
+++ settings.php
@@ -3,12 +3,18 @@
 /**
  * This file is used for system-wide global settings.
  *
- * If add a setting, be aware that keys are strings and settings must be scalar.
+ * If add a setting, be aware that keys are strings and settings must be scalar
+ * or array.
  */
 
 $_SETTINGS = [
     'logfile' => __DIR__ . '/log',
     'pagename' => 'ASPR Skeleton Webpage.',
+];
+
+// In some very rare instances, want to use different settings for tests.
+$_TEST_SETTINGS = [
+    'pagename' => 'ASPR Test Webpage.',
 ];
 
 /**
@@ -20,8 +26,21 @@ $_SETTINGS = [
  */
 function getSetting(string $key) {
     global $_SETTINGS;
-    $returnVal = $_SETTINGS[$key];
-    if (!is_scalar($returnVal)) {
+    global $_TEST_SETTINGS;
+
+    $useTest =
+        ($GLOBALS['isTest'] ?? false) &&
+        array_key_exists($key, $_TEST_SETTINGS) &&
+        array_key_exists($key, $_SETTINGS) // Want it to be available in *both* settings and test settings.
+    ;
+
+    if ($useTest) {
+        $returnVal = $_TEST_SETTINGS[$key];
+    } else {
+        $returnVal = $_SETTINGS[$key];
+    }
+
+    if (!is_scalar($returnVal) && !is_array($returnVal)) {
         throw new Exception('Setting cannot be non-scalar.  Found '
         . gettype($returnVal));
     }
