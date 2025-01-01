29d2aecd3bb3c8ab0efe36d5a2722c12e96756bc
Andrew Norman <anorman728@gmail.com>
2025-01-01 12:18:53 -0600

    Updated tests to match inane PHPUnit changes.

--- tests/SampleControllerTest.php
+++ tests/SampleControllerTest.php
@@ -38,7 +38,7 @@ class SampleControllerTest extends TestCase
         // string.
         $html = $this->getHtml($this->sampleController, 'home');
 
-        $this->assertRegexp(
+        $this->assertMatchesRegularExpression(
             '/include_once.*View/s', // I keep forgetting that stupid "s" flag.
             $html,
             'Asserting that some values are found in the html.'
@@ -59,7 +59,7 @@ class SampleControllerTest extends TestCase
             ['my argument']
         );
 
-        $this->assertContains(
+        $this->assertStringContainsString(
             'you passed "my argument"',
             $html,
             'Asserting that argument is returned in page html.'
